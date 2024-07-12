3a6ff0d7322346d87616c622612f7e96835e73d0
Watanabe Takashi <wtnb75@gmail.com>
2024-07-13 00:14:11 +0900

    試せるように

--- pstyle/main.py
+++ pstyle/main.py
@@ -1,7 +1,9 @@
 import click
 import functools
 import json
-from typing import Any
+import sys
+from urllib.parse import urlparse, ParseResult, parse_qsl
+from typing import Any, Callable
 from logging import getLogger
 from .version import VERSION
 from .convert import Pstyle, styles
@@ -51,6 +53,175 @@ def convert(operation, args, kwargs, from_style, to_style, normalize):
     _log.debug("SQL(after): %s, args=%s", result_op, result_args)
     click.echo(f"op: {result_op}")
     click.echo(f"args: {result_args}")
+
+
+dbapis: dict[str, tuple[str, Callable[[ParseResult], Any]]] = {}
+
+try:
+    import sqlite3
+
+    def sqlite3_connect(u: ParseResult):
+        return sqlite3.connect(u.netloc+u.path)
+    dbapis["sqlite3"] = (sqlite3.paramstyle, sqlite3_connect)
+except ImportError:
+    pass
+try:
+    import mysql.connector
+
+    def mysql_connect(u: ParseResult):
+        return mysql.connector.connect(
+            host=u.hostname, port=u.port, user=u.username, password=u.password,
+            database=u.path.lstrip("/"))
+    dbapis["mysql"] = (mysql.connector.paramstyle, mysql_connect)
+except ImportError:
+    pass
+try:
+    import mariadb
+
+    def mariadb_connect(u: ParseResult):
+        return mariadb.connect(
+            host=u.hostname, port=u.port, user=u.username, password=u.password,
+            database=u.path.lstrip("/"))
+    dbapis["mariadb"] = (mariadb.paramstyle, mariadb_connect)
+except ImportError:
+    pass
+try:
+    import psycopg2
+
+    def psql_connect(u: ParseResult):
+        return psycopg2.connect(
+            host=u.hostname, port=u.port, user=u.username, password=u.password,
+            dbname=u.path.lstrip("/"))
+    dbapis["postgres"] = (psycopg2.paramstyle, psql_connect)
+except ImportError:
+    pass
+try:
+    import pyodbc
+
+    def odbc_connect(u: ParseResult):
+        params = dict(parse_qsl(u.query))
+        return pyodbc.connect(u.path.lstrip("/"), **params)
+    dbapis["odbc"] = (pyodbc.paramstyle, odbc_connect)
+except ImportError:
+    pass
+try:
+    import pymssql
+
+    def mssql_connect(u: ParseResult):
+        params = dict(parse_qsl(u.query))
+        return pymssql.connect(
+            host=u.hostname, port=u.port, user=u.username, password=u.password,
+            database=u.path.lstrip("/"), **params)
+    dbapis["mssql"] = (pymssql.paramstyle, mssql_connect)
+except ImportError:
+    pass
+try:
+    import oracledb
+
+    def oracle_connect(u: ParseResult):
+        params = dict(parse_qsl(u.query))
+        return oracledb.connect(
+            host=u.hostname, port=u.port, user=u.username, password=u.password,
+            service_name=u.path.lstrip("/"), **params)
+    dbapis["oracle"] = (oracledb.paramstyle, oracle_connect)
+except ImportError:
+    pass
+try:
+    import duckdb
+
+    def duckdb_connect(u: ParseResult):
+        params = dict(parse_qsl(u.query))
+        return duckdb.connect(u.netloc+u.path, config=params)
+    dbapis["duckdb"] = (duckdb.paramstyle, duckdb_connect)
+except ImportError:
+    pass
+try:
+    import firebird.driver
+
+    def firebird_connect(u: ParseResult):
+        return firebird.driver.connect(
+            database=f"{u.hostname}:{u.port}{u.path}", user=u.username, password=u.password)
+    dbapis["firebird"] = (firebird.driver.paramstyle, firebird_connect)
+except ImportError:
+    pass
+try:
+    # XXX: pydrda can't execute with paramters against derby
+    import drda
+
+    def drda_connect(u: ParseResult):
+        return drda.connect(
+            host=u.hostname, port=u.port, user=u.username, password=u.password,
+            database=u.path.lstrip("/")+";create=true")
+    dbapis["drda"] = (drda.paramstyle, drda_connect)
+except ImportError:
+    pass
+try:
+    import pymonetdb
+
+    def monetdb_connect(u: ParseResult):
+        return pymonetdb.connect(
+            hostname=u.hostname, port=u.port, username=u.username, password=u.password,
+            database=u.path.lstrip("/"))
+    dbapis["monetdb"] = (pymonetdb.paramstyle, monetdb_connect)
+except ImportError:
+    pass
+try:
+    import pyhive.hive
+
+    def hive_connect(u: ParseResult):
+        params = dict(parse_qsl(u.query))
+        return pyhive.hive.connect(
+            host=u.hostname, port=u.port, username=u.username, password=u.password,
+            database=u.path.lstrip("/"), **params)
+    dbapis["hive"] = (pyhive.hive.paramstyle, hive_connect)
+except ImportError:
+    pass
+try:
+    import pyhive.presto
+
+    def presto_connect(u: ParseResult):
+        params = dict(parse_qsl(u.query))
+        return pyhive.presto.connect(
+            host=u.hostname, port=u.port, username=u.username, password=u.password,
+            schema=u.path.lstrip("/"), **params)
+    dbapis["presto"] = (pyhive.presto.paramstyle, presto_connect)
+except ImportError:
+    pass
+
+
+@cli.command()
+@verbose_option
+@click.option("--output", type=click.File('w'), default=sys.stdout)
+def list_drivers(output):
+    json.dump(list(dbapis.keys()), output)
+
+
+@cli.command()
+@verbose_option
+@click.option("--style", type=click.Choice(styles+["auto"]), default="auto", show_default=True)
+@click.option("--normalize/--original", default=True, show_default=True)
+@click.option("--ipython/--code", default=True, show_default=True)
+@click.argument("dsn")
+def try_db(dsn, style, normalize, ipython):
+    if ipython:
+        import IPython
+    else:
+        import readline  # noqa
+        import code
+    from .wrapper import DBWrapper
+    parsed = urlparse(dsn)
+    if parsed.scheme not in dbapis:
+        raise click.BadArgumentUsage(f"{parsed.scheme} not found in {list(dbapis.keys())}")
+    paramstyle, connector = dbapis.get(parsed.scheme)
+    db = connector(parsed)
+    wrapped = DBWrapper(db, paramstyle, style, normalize)
+    click.echo(f"db({paramstyle}): db.execute(...)")
+    click.echo(f"wrapped({style}): wrapped.execute(...)")
+    names = {"dsn": dsn, "db": db, "wrapped": wrapped, "paramstyle": (paramstyle, style), "version": VERSION}
+    if ipython:
+        IPython.start_ipython(argv=[], user_ns=names)
+    else:
+        code.InteractiveConsole(locals=names).interact()
 
 
 if __name__ == "__main__":
--- pstyle/wrapper.py
+++ pstyle/wrapper.py
@@ -4,20 +4,21 @@ from .convert import Pstyle
 class CursorWrapper:
     """wrapper of DB cursor object"""
 
-    def __init__(self, cursor, orig_paramstyle: str, paramstyle: str):
+    def __init__(self, cursor, orig_paramstyle: str, paramstyle: str, normalize: bool = True):
         self._cursor = cursor
         self._orig_paramstyle = orig_paramstyle
         self._paramstyle = paramstyle
+        self.normalize = normalize
 
     def execute(self, operation, parameters=()):
-        op, a = Pstyle.convert(self._paramstyle, self._orig_paramstyle, operation, parameters)
+        op, a = Pstyle.convert(self._paramstyle, self._orig_paramstyle, operation, parameters, self.normalize)
         return self._cursor.execute(op, a)
 
     def executemany(self, operation, seq_of_parameters=[]):
         op = None
         sop = []
         for p in seq_of_parameters:
-            op1, p1 = Pstyle.convert(self._paramstyle, self._orig_paramstyle, operation, p)
+            op1, p1 = Pstyle.convert(self._paramstyle, self._orig_paramstyle, operation, p, self.normalize)
             if op is None:
                 op = op1
             assert op == op1
@@ -33,13 +34,14 @@ class CursorWrapper:
 class DBWrapper:
     """wrapper of DB connection object"""
 
-    def __init__(self, db, orig_paramstyle: str, paramstyle: str):
+    def __init__(self, db, orig_paramstyle: str, paramstyle: str, normalize: bool = True):
         self._db = db
         self.paramstyle = paramstyle
         self._orig_paramstyle = orig_paramstyle
+        self.normalize = normalize
 
     def cursor(self):
-        return CursorWrapper(self._db.cursor(), self._orig_paramstyle, self.paramstyle)
+        return CursorWrapper(self._db.cursor(), self._orig_paramstyle, self.paramstyle, self.normalize)
 
     def execute(self, operation, parameters=()):
         return self.cursor().execute(operation, parameters)
